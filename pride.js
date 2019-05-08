2a528c95dfc4ecc9570932dd32e5b375d2c6213a
sorz <me@sorz.org>
2019-05-08 11:57:44 +0800

    Update light color scheme

--- pride.js
+++ pride.js
@@ -1,7 +1,7 @@
 'use strict';
 const COLOR_SCHEMES = {
     'standard': ['#e50000', '#ff8d00', '#ffee00', '#008121', '#004cff', '#760188'],
-    'light':    ['#ff3333', '#ffaf4d', '#fff34d', '#00ce35', '#4d82ff', '#b802d4'],
+    'modern':   ['#E53935', '#FB8C00', '#FDD835', '#43A047', '#1E88E5', '#8E24AA'],
     'trans':    ['#5bcffa', '#f5abb9', '#ffffff', '#f5abb9', '#5bcffa']
 };
 const canvas = document.getElementById('canvas');
